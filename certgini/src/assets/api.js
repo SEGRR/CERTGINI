81ddb54cb4bf5317050fb82143edce0111af3e19
segrr <shaikhsufiyan2003@gmail.com>
2024-04-04 15:44:19 +0530

    updated deployed url

--- certgini/src/assets/api.js
+++ certgini/src/assets/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// axios.defaults.withCredentials =true;
-const url = 'http://localhost:8181'
+axios.defaults.withCredentials =true;
+const url = "https://certgini-api.vercel.app/";
 export async function makeExportRequest(formData , id){
  let res = await axios.post(`${url}/export/${id}` , formData , {responseType:'blob'});
   return res;
